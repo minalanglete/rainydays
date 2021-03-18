0b069e2e1f38e571d34cf48b496df3802949c6f0
mina <71304403+minalanglete@users.noreply.github.com>
2021-03-18 14:47:58 +0100

    test form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const form = document.querySelector("#contactForm");
 const theName = document.querySelector("#name");
 const nameError = document.querySelector("#nameError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
-
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
 
 function validateForm (event) {
     event.preventDefault();
@@ -14,12 +17,24 @@ function validateForm (event) {
         nameError.style.display = "block";
     }
 
+    if (subject.value.trim().length > 10) {
+        subjectError.style.display = "none";
+    } else {
+        subjectError.style.display = "block";
+    }
+
     if (validateEmail(email.value) === true) {
         emailError.style.display = "none";
     } else {
         emailError.style.display = "block";
     }
 
+    if (address.value.trim().length > 25) {
+        addressError.style.display = "none";
+    } else {
+        addressError.style.display = "block";
+    }
+    
 }
 
 form.addEventListener("submit", validateForm);
